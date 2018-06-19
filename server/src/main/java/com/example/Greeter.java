f7dd37f4dc575b81f5512690f8711b25022c95c6
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2018-06-20 00:35:26 +0300

    Some changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * A method that greets person
+   *
+   * @param someone A person to greet
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
